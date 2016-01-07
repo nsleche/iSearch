81db4216c947b0ecf22fe3c435eb9d49498a2894
chucky <antonioalvesdev@gmail.com>
2016-01-07 21:32:09 -0200

    Added search bar and table view

---
+++ iSearch/SearchResults.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResults.swift
+//  iSearch
+//
+//  Created by Antonio Alves on 1/7/16.
+//  Copyright © 2016 Antonio Alves. All rights reserved.
+//
+
+import Foundation
+
+
+class SearchResult {
+    
+    var name = ""
+    var artistName = ""
+    
+}
--- iSearch/SearchViewController.swift
+++ iSearch/SearchViewController.swift
@@ -10,26 +10,88 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var searchResults = [SearchResult]()
+    var hasSearched = false
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+
+}
+
+
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchResults = [SearchResult]()
+        searchBar.resignFirstResponder()
+        
+        if searchBar.text! != "justin bieber" {
+            for i in 0...2 {
+                let result = SearchResult()
+                result.name = String(format:"Fake result %d for", i)
+                result.artistName = searchBar.text!
+                searchResults.append(result)
+                hasSearched = true
+            }
+        }
+        tableView.reloadData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
+        return .TopAttached
     }
-    */
+}
 
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !hasSearched {
+            return 0
+        } else if searchResults.count == 0 {
+            return 1
+        } else {
+            return searchResults.count
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier("Cell")
+        if cell == nil {
+            cell = UITableViewCell(style: .Subtitle, reuseIdentifier: "Cell")
+        }
+        
+        if searchResults.count == 0 {
+            cell.textLabel?.text = "(Nothing Found)"
+            cell.detailTextLabel?.text = ""
+        } else {
+            let result = searchResults[indexPath.row]
+            cell.textLabel?.text = result.name
+            cell.detailTextLabel?.text = result.artistName
+        }
+        return cell
+    }
+    
+}
+
+extension SearchViewController: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        if searchResults.count == 0 {
+            return nil
+        } else {
+            return indexPath
+        }
+    }
+    
 }
