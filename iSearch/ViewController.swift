b0a041edbb01b5f5744408d6bd082ba8df9bf561
chucky <antonioalvesdev@gmail.com>
2016-01-07 20:51:12 -0200

    Initial Commit

---
+++ iSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iSearch
+//
+//  Created by Antonio Alves on 1/7/16.
+//  Copyright © 2016 Antonio Alves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
